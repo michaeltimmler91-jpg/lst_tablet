6f89840f5e493fbb50bff210a82fcde81de4d257
michaeltimmler91-jpg <michaeltimmler91@googlemail.com>
2026-07-07 21:44:32 +0200

    Add taxi tablet resource manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Los Santos Taxi'
+description 'Los Santos Taxi Tablet'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
+client_script 'client.lua'
